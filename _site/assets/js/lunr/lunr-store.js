936994b5fd748e53ba53850bd5d02eb9b0cefda0
Mr. T. <tcarduck@gmx.de>
2025-01-02 17:22:33 +0100

    new _config.yml

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2025/01/02/welcome-to-jekyll.html",
+        "url": "/jekyll/update/welcome-to-jekyll/",
         "teaser": null
       },{
         "title": "Second Test Post",
         "excerpt":"Test der Syntax Code snippets: def print_hi(name) puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2025/01/02/Second-Test-Posting.html",
+        "url": "/jekyll/update/Second-Test-Posting/",
         "teaser": null
       }]
