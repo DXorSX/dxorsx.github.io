6af284b416d5b02a433013f5b4af06a0c20b38a3
Mr. T. <tcarduck@gmx.de>
2025-01-02 17:56:17 +0100

    Template

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,11 +3,11 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/jekyll/update/welcome-to-jekyll/",
-        "teaser": null
+        "teaser": "/assets/images/500x300_teaser.jpg"
       },{
         "title": "Second Test Post",
         "excerpt":"Test der Syntax Code snippets: def print_hi(name) puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/jekyll/update/Second-Test-Posting/",
-        "teaser": null
+        "teaser": "/assets/images/500x300_teaser.jpg"
       }]
