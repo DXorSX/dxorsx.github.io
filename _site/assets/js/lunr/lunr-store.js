2cbc2c9b9689fd72f877458f88e051ccad876be6
Mr. T. <tcarduck@gmx.de>
2025-01-02 17:02:28 +0100

    changed theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/01/02/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Second Test Post",
+        "excerpt":"Test der Syntax Code snippets: def print_hi(name) puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT. Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/01/02/Second-Test-Posting.html",
+        "teaser": null
+      }]
